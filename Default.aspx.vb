a2435972b1dac5fa2e399a7b0d7a62cc1c58aaed
Grafritz <duverseau.jeanfritz@gmail.com>
2018-04-05 15:10:26 -0500

    Mise a jour vers le serveur IHSI

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -34,11 +34,11 @@ Partial Class _Default
                 Literal_Titre.Text = [Global].Global_APP_NAME_SIGLE
                 Literal_TitrePage.Text = "APP APK STORE IHSI"
 
-                Link_NEW_IHSI_rgph_Ver02Avril2018.Attributes.Add("href", [Global].Global_FTP_ADRESSE & "IHSI_5eRGPH/IHSI-rgph-Ver1.0_02-04-2018/IHSI-rgph-Ver1.0_02-04-2018.apk")
-                Link_NEW_IHSI_rgph_Ver1.Attributes.Add("href", [Global].Global_FTP_ADRESSE & "IHSI_5eRGPH/NEW_IHSI-rgph-Ver1.0_26-03-2018/NEW_IHSI-rgph-Ver1.0_26-03-2018.apk")
+                Link_NEW_IHSI_rgph_Ver02Avril2018.Attributes.Add("href", [Global].Global_FTP_ADRESSE & [Global].Global_PATH_FOLDER_APK & [Global].Global_RGPH_FOLDER_NAME & "IHSI-rgph-Ver1.0_02-04-2018/IHSI-rgph-Ver1.0_02-04-2018.apk")
+                Link_NEW_IHSI_rgph_Ver1.Attributes.Add("href", [Global].Global_FTP_ADRESSE & [Global].Global_PATH_FOLDER_APK & [Global].Global_RGPH_FOLDER_NAME & "NEW_IHSI-rgph-Ver1.0_26-03-2018/NEW_IHSI-rgph-Ver1.0_26-03-2018.apk")
 
 
-                Link_RGPH_Formation_V1.Attributes.Add("href", [Global].Global_FTP_ADRESSE & "RGPH_FormationEvaluation/RGPH_Formation_V1/RGPH_Formation_V1.apk")
+                Link_RGPH_Formation_V1.Attributes.Add("href", [Global].Global_FTP_ADRESSE & [Global].Global_PATH_FOLDER_APK & "RGPH_FormationEvaluation/RGPH_Formation_V1/RGPH_Formation_V1.apk")
 
                 'BindGrid()
             End If
